71540042df65dee1411a519fdc8b05ab0b77baf0
Redblood639 <albinorabbithunter@gmail.com>
2022-03-20 05:41:17 +0800

    feature: fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import { getNFTCollections } from "./utils/index";
+import _ from "lodash";
 import "./App.css";
 
 function App() {
   const [address, setAddress] = useState<string>("");
-  const onHandle = async () => {
-    let data = [];
-    if (address !== "") {
-      data = await getNFTCollections(address);
-    }
+  // @type  0 - start, 1 - running, 2- end
+  const [status, setStatus] = useState<boolean>(false);
+  const [data, setData] = useState<any>([]);
 
-    console.log(data);
+  const onHandle = async () => {
+    setStatus(true);
+
+    let pack: any = [];
+    if (address !== "") {
+      pack = await getNFTCollections(address);
+    } else {
+      alert("Please input correct address.");
+    }
+    setStatus(false);
+    setData(pack);
+    console.log(pack);
   };
+
   return (
     <div className="App">
-      <input
-        type={"text"}
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <button onClick={onHandle}>{"GET"}</button>
+      <div>
+        <input
+          type={"text"}
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <button onClick={onHandle}>{"GET"}</button>
+      </div>
+      <div>
+        {status ? "Searching" : _.isEmpty(data) ? "Nothing Found" : "Success"}
+      </div>
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,62 @@
 import { Connection } from "@metaplex/js";
-import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
-import axios from "axios";
+import {
+  Metadata,
+  MetadataData,
+} from "@metaplex-foundation/mpl-token-metadata";
 
+const SOLSNATCHERS_CANDY_MACHINES = [
+  "7jdtxdmiQTrAeCqQkuvtJiiZ7Pe7SHkQucEeLPJKHX3q",
+  "85Sfoc13utW3rwMPJTQKd5rqPym9rVq4Roh4zKKcgsK6",
+  "EeoKFLM9Cc1QAbdR3RFWcbZ9bP8CjQ2nWoNH8cJNeKq2",
+  "BHZXW8W7w84kQRRi3W7hyYjinFuGT9LPYL88bM398vEs",
+  "6fW2M3dJfUbLL25V74x5XYdQNajjdo56uKosLUTMcsLi",
+];
+
+export const fetchNFTs = (nftsmetadata: MetadataData[]) => {
+  const filteredMetadataAccounts = nftsmetadata?.filter((metadataAccount) => {
+    const { data } = metadataAccount;
+
+    const creator = data?.creators?.find(
+      (creator) => SOLSNATCHERS_CANDY_MACHINES.indexOf(creator.address) !== -1
+    );
+
+    /** Make sure it's verified to prevent exploiters */
+    if (creator && creator.verified) {
+      return metadataAccount;
+    }
+    return false;
+  });
+
+  return filteredMetadataAccounts;
+};
 export const getNFTCollections = async (OwnerPublickey: string) => {
   const connection = new Connection("mainnet-beta");
-  let data = [];
+  let data: any = [];
 
   try {
-    const nftsmetadata = await Metadata.findDataByOwner(
+    const nftsmetadata: MetadataData[] = await Metadata.findDataByOwner(
       connection,
       OwnerPublickey
     );
-    for (let i = 0; i < nftsmetadata.length; i++) {
-      const offchain = await axios.get(nftsmetadata[i].data.uri);
-      data.push(offchain.data);
-    }
+
+    const filteredMetadataAccounts = fetchNFTs(nftsmetadata);
+    if (!filteredMetadataAccounts || !filteredMetadataAccounts.length)
+      return [];
+
+    const metadataPromises = filteredMetadataAccounts.map(async (metadata) => {
+      const {
+        data: { uri },
+      } = metadata;
+      const content = await (await fetch(uri)).json();
+
+      return {
+        onChain: metadata,
+        offChain: content,
+      };
+    });
+
+    data = await Promise.all(metadataPromises);
+
     return data;
   } catch (e) {
     return data;
